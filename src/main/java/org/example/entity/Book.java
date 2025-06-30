f06c99a58ad95844ca53ca26e588d919577c7367
kryshtal <shimorowm@gmail.com>
2025-06-30 18:04:33 +0300

    feat: project structure

---
+++ src/main/java/org/example/entity/Book.java
@@ -0,0 +1,14 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Book {
+    @Id
+    private Long id;
+}
