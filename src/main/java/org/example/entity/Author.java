29f53fbf3dd5e638e7bcd845162f8d744a1f87ff
22crystyle <22crystyle@gmail.com>
2025-07-09 13:44:00 +0300

    feat: project structure

---
+++ src/main/java/org/example/entity/Author.java
@@ -0,0 +1,14 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Author {
+    @Id
+    private Long id;
+}
